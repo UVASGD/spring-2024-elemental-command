e93edbb11d4f6b8a8dcbe2c83d2426659ef97b70
arthuroker <arthur.oker@gmail.com>
2024-04-07 16:02:05 -0400

    breakable wall on selected speed threshold and earth activated, added to prefab folder
    also changed credits screen "back" to "main menu" cause it brings you there anyway

---
+++ Assets/Scripts/BreakableWall/DestoryWall.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour
+{
+    public float collisionSpeedThreshold = 5f; // Define the collision speed threshold here
+
+        private ElementManager em;
+
+    void Start()
+    {
+        em = FindObjectOfType<ElementManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("impact was made");
+        if (collision.gameObject.CompareTag("Player") && em.state == ElementManager.Element.Earth)
+        {
+            // Check if the collision velocity magnitude is greater than the threshold
+            if (collision.relativeVelocity.magnitude >= collisionSpeedThreshold)
+            {
+                Destroy(gameObject); // Destroy the game object
+            }
+        }
+    }
+}
